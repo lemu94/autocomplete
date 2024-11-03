c14f6f9e2a68260b47ac7da78a011d0e9934a2ce
YOKOI KPAMISSA LEMUEL-ANGE <lyokoi@reference.ca>
2024-11-03 08:58:19 -0500

    improve performance + add description

--- autocomplete.component.ts
+++ autocomplete.component.ts
@@ -4,7 +4,7 @@ import { AsyncPipe } from '@angular/common';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { EMPTY, map, Observable, startWith } from "rxjs";
+import { debounceTime, distinctUntilChanged, EMPTY, map, Observable, startWith } from "rxjs";
 import { MatIconModule } from "@angular/material/icon";
 
 /**
@@ -26,6 +26,20 @@ type configField<T> = {
     get: keyof T,
 };
 
+/**
+ * Custom autocomplete component.
+ * This component provides an abstraction layer to the base Angular Material Autocomplete component.
+ *
+ * @example
+ * <app-autocomplete 
+ *   [listeData]="liste_personn" 
+ *   [configField]="{'show': 'nom', 'filter': 'nom', 'get': 'adresse'}"
+ *   [label]="'liste des personnes'"
+ *   (onChangeValue)="valeur($event)">
+ * </app-autocomplete>
+ */
+
+
 @Component({
     selector: "app-autocomplete",
     templateUrl: "./autocomplete.component.html",
@@ -42,6 +56,8 @@ type configField<T> = {
     standalone: true
 })
 export class AutoCompleteComponent<T> implements OnInit {
+
+    
     /**
      * List of data to display in autocomplete.
      */
@@ -75,6 +91,9 @@ export class AutoCompleteComponent<T> implements OnInit {
     filteredOptions: Observable<T[]> = EMPTY;
 
     ngOnInit(): void {
+        if (!Array.isArray(this.listeData) || this.listeData.some(item => typeof item !== 'object' || item === null)) {
+            throw new Error('listeData must be an array of non-null objects');
+          }
         this.onFocusControlAuto();
     }
 
@@ -84,6 +103,8 @@ export class AutoCompleteComponent<T> implements OnInit {
     onFocusControlAuto() {
         this.filteredOptions = this.myControlAuto.valueChanges.pipe(
             startWith(''),
+            debounceTime(300), 
+            distinctUntilChanged(), 
             map(value => this._filter(value as string)),
         );
 
